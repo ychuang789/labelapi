ff9ffb1607705846e6603ab3aee08d2cc37aac06
Weber <ychuang@eland.com.tw>
2021-10-04 11:14:15 +0800

    add keyword base model

--- definition.py
+++ definition.py
@@ -2,3 +2,7 @@ from pathlib import Path, PurePath
 
 ROOT_DIR = PurePath(__file__).parent
 ROOT_LOG_DIR = Path(ROOT_DIR / "logs")
+
+
+DEEPNLP_POS_API = "http://127.0.0.1/segment"
+DEEPNLP_POS_API_TOKEN = ""
