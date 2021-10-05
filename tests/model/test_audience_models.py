3ba0252031e68bcb2892b1bf222f25f66b47363a
Weber <ychuang@eland.com.tw>
2021-10-05 17:11:59 +0800

    edit test script

--- tests/model/test_audience_models.py
+++ tests/model/test_audience_models.py
@@ -2,19 +2,17 @@ from datetime import datetime
 from unittest import TestCase
 
 from models.audience_models import RuleModel
-from settings import PredictTarget
+from utils.selections import PredictTarget
 from utils.input_example import InputExample
 
 post_male = "小弟我沒女友，在八卦版混了很久了在八卦版混，時常會有的福利就是偶爾會有人貼清涼養眼圖上班上久了，心情煩悶，看這些圖多多少少會有解鬱消悶的效果感謝八卦版大的貢獻所以在八卦版上，只要看到巨乳等關鍵字，我都是會直覺地點閱進入觀賞，以調劑身心。"
 post_female = "小妹的朋友上週領薪水這週就跟我哭哭說全身上下剩一千五明明是她自己限制自己一個月只能花幾千塊其他買保險跟存起來這樣有什麼好哭哭的就是自己強迫自己啊而且明明就是存起來哪是沒錢啊不懂欸"
 post_female_2 = "小妹我男朋友上週領薪水這週就跟我哭哭說全身上下剩一千五明明是她自己限制自己一個月只能花幾千塊其他買保險跟存起來這樣有什麼好哭哭的就是自己強迫自己啊而且明明就是存起來哪是沒錢啊不懂欸"
 post_young = "小弟我出社會工作不到兩年，今天辦了人生第一張信用卡...。"
-input_male = InputExample(id_="2", s_area_id="2", author="Bob", title="", content=post_male,
-                          post_time=datetime.now(), label="male")
+
 input_female = InputExample(id_="1", s_area_id="ptt_woman_talk", author="Alice", title="", content=post_female,
                             post_time=datetime.now(), label="female")
-input_female_2 = InputExample(id_="1", s_area_id="ptt_woman_talk", author="Alice", title="", content=post_female_2,
-                              post_time=datetime.now(), label="female")
+
 input_young = InputExample(id_="1", s_area_id="1", author="Alice", title="", content=post_young,
                            post_time=datetime.now())
 
