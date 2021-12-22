5aee7b1143274d5265048b307738fd17fc292a5c
Weber <ychuang@eland.com.tw>
2021-12-22 15:42:04 +0800

    create model creator and test it

---
+++ models/model_creator.py
@@ -0,0 +1,55 @@
+from models.keyword_model import KeywordModel
+from models.rf_model import RandomForestModel
+from models.rule_model import RuleModel
+from models.tw_model import TermWeightModel
+from utils.selections import ModelType
+
+class ModelTypeNotFound(Exception):
+    pass
+
+class ParamterMissingError(Exception):
+    pass
+
+class ModelCreator():
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return 'Generate a corresponded model which user called'
+
+    @staticmethod
+    def create_model(type_attribute: ModelType.KEYWORD_MODEL, **kwargs):
+
+        if not hasattr(ModelType, type_attribute):
+            raise ModelTypeNotFound(f'{type_attribute} is not in default ModelType')
+        else:
+            type_attribute = type_attribute.lower()
+
+
+        if type_attribute == ModelType.KEYWORD_MODEL.value:
+            keyword_patterns = kwargs.get('keyword_patterns')
+            if not keyword_patterns:
+                raise ParamterMissingError(f'keyword patterns is missing')
+            return KeywordModel(label_keywords=keyword_patterns)
+
+        if type_attribute == ModelType.RULE_MODEL.value:
+            regex_patterns = kwargs.get('regex_patterns')
+            if not regex_patterns:
+                raise ParamterMissingError(f'regex patterns is missing')
+            return RuleModel(model_rules=regex_patterns)
+
+        if type_attribute == ModelType.RANDOM_FOREST_MODEL.value:
+            model_path = kwargs.get('model_path')
+            if not model_path:
+                raise ParamterMissingError(f'model_path is missing')
+            return RandomForestModel(model_dir_name=model_path)
+
+        if type_attribute == ModelType.TERM_WEIGHT_MODEL.value:
+            model_path = kwargs.get('model_path')
+            if not model_path:
+                raise ParamterMissingError(f'model_path is missing')
+            return TermWeightModel(model_dir_name=model_path)
+
+
+
