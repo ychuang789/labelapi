8ba8070cf8c5ab93f0ce8b890b84ece09a96991b
Weber <ychuang@eland.com.tw>
2022-03-30 12:05:04 +0800

    edit get batch func

--- utils/database/database_helper.py
+++ utils/database/database_helper.py
@@ -154,7 +154,7 @@ def get_batch_by_timedelta(schema, predict_type, table,
                 begin_date += interval
                 cursor.close()
             except Exception as e:
-                # yield e, begin_date
+                yield e, begin_date
                 connection.close()
                 raise e
 
