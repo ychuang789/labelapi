f8cf4f593e5b5515afa2017395eff4c3e515b417
Weber <ychuang@eland.com.tw>
2022-04-19 18:11:13 +0800

    finished document crud API

--- apis/documenting_api.py
+++ apis/documenting_api.py
@@ -8,7 +8,6 @@ from fastapi.responses import JSONResponse
 from apis.input_class.documenting_input import DocumentRequest
 from settings import DatabaseConfig
 from utils.enum_config import DocumentDatasetType, DocumentRulesType
-from utils.exception_manager import SessionError
 from workers.orm_core.document_operation import DocumentCRUD
 
 router = APIRouter(prefix='/documents',
@@ -98,7 +97,7 @@ def dataset_add(task_id: str, body: DocumentDatasetType):
     conn = DocumentCRUD(connection_info=DatabaseConfig.OUTPUT_ENGINE_INFO)
     try:
         dataset_id = conn.dataset_create(task_id=task_id, **body.__dict__)
-        return JSONResponse(status_code=status.HTTP_200_OK, content=f'OK, set id as {dataset_id}')
+        return JSONResponse(status_code=status.HTTP_200_OK, content=f'OK, data {dataset_id} is created')
     except Exception as e:
         err_msg = f'failed to create a data since {type(e).__name__}:{e}'
         return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
@@ -108,23 +107,79 @@ def dataset_add(task_id: str, body: DocumentDatasetType):
 
 
 @router.patch('/{task_id}/dataset/{dataset_id}/update', description='update single data')
-def dataset_update(task_id: str, dataset_id: str, body: DocumentDatasetType):
+def dataset_update(task_id: str, dataset_id: int, body: DocumentDatasetType):
+    conn = DocumentCRUD(connection_info=DatabaseConfig.OUTPUT_ENGINE_INFO)
+    try:
+        dataset_id = conn.dataset_update(task_id=task_id, dataset_id=dataset_id, **body.__dict__)
+        return JSONResponse(status_code=status.HTTP_200_OK,
+                            content=f'OK, data {dataset_id} is updated')
+    except Exception as e:
+        err_msg = f'failed to update a data since {type(e).__name__}:{e}'
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content=jsonable_encoder(err_msg))
+    finally:
+        conn.dispose()
+
+
+@router.delete('/{task_id}/dataset/{dataset_id}/delete', description='delete single data')
+def dataset_delete(task_id: str, dataset_id: int):
+    conn = DocumentCRUD(connection_info=DatabaseConfig.OUTPUT_ENGINE_INFO)
+    try:
+        conn.dataset_delete(task_id=task_id, dataset_id=dataset_id)
+        return JSONResponse(status_code=status.HTTP_200_OK,
+                            content=f'OK, data {dataset_id} is deleted')
+    except Exception as e:
+        err_msg = f'failed to delete a data since {type(e).__name__}:{e}'
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content=jsonable_encoder(err_msg))
+    finally:
+        conn.dispose()
     pass
 
 
-@router.post('/{task_id}/rules/add', description='add single data')
-def dataset_add(task_id: str, body: DocumentRulesType):
-    pass
+@router.post('/{task_id}/rules/add', description='add single rule')
+def rule_add(task_id: str, body: DocumentRulesType):
+    conn = DocumentCRUD(connection_info=DatabaseConfig.OUTPUT_ENGINE_INFO)
+    try:
+        rule_id = conn.rule_create(task_id=task_id, **body.__dict__)
+        return JSONResponse(status_code=status.HTTP_200_OK,
+                            content=f'OK, rule {rule_id} is created')
+    except Exception as e:
+        err_msg = f'failed to create a rule since {type(e).__name__}:{e}'
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content=jsonable_encoder(err_msg))
+    finally:
+        conn.dispose()
 
 
 @router.patch('/{task_id}/rules/{rule_id}/update', description='update single data')
-def dataset_update(task_id: str, rule_id: str, body: DocumentRulesType):
-    pass
+def dataset_update(task_id: str, rule_id: int, body: DocumentRulesType):
+    conn = DocumentCRUD(connection_info=DatabaseConfig.OUTPUT_ENGINE_INFO)
+    try:
+        rule_id = conn.rule_update(task_id=task_id, rule_id=rule_id, **body.__dict__)
+        JSONResponse(status_code=status.HTTP_200_OK,
+                     content=f'OK, rule {rule_id} is updated')
+    except Exception as e:
+        err_msg = f'failed to update a rule since {type(e).__name__}:{e}'
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content=jsonable_encoder(err_msg))
+    finally:
+        conn.dispose()
 
 
 @router.delete('/{task_id}/rules/{rule_id}/delete', description='delete single data')
-def dataset_delete(task_id: str, rule_id: str):
-    pass
+def dataset_delete(task_id: str, rule_id: int):
+    conn = DocumentCRUD(connection_info=DatabaseConfig.OUTPUT_ENGINE_INFO)
+    try:
+        conn.rule_delete(task_id=task_id, rule_id=rule_id)
+        JSONResponse(status_code=status.HTTP_200_OK,
+                     content=f'OK, rule {rule_id} is deleted')
+    except Exception as e:
+        err_msg = f'failed to delete a rule since {type(e).__name__}:{e}'
+        return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            content=jsonable_encoder(err_msg))
+    finally:
+        conn.dispose()
 
 
 @router.post('/{task_id}/dataset/upload/{overwrite}', description='upload and overwrite dataset')
